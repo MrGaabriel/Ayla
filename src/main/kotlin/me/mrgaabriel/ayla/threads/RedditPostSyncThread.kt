ef9d939c41f8dd9ce7bdea2a95f4c2d047922bf0
Gabriel Bellot <gabrielbellot7@gmail.com>
2018-08-18 21:18:13 -0300

    Async, corrigir bugs

--- src/main/kotlin/me/mrgaabriel/ayla/threads/RedditPostSyncThread.kt
+++ src/main/kotlin/me/mrgaabriel/ayla/threads/RedditPostSyncThread.kt
@@ -4,6 +4,7 @@ import com.github.kevinsawicki.http.*
 import com.github.salomonbrys.kotson.*
 import com.google.common.flogger.*
 import com.google.gson.*
+import kotlinx.coroutines.experimental.*
 import me.mrgaabriel.ayla.utils.*
 import net.dv8tion.jda.core.*
 import org.apache.commons.lang3.exception.*
@@ -31,68 +32,75 @@ class RedditPostSyncThread : Thread("Reddit Posts Sync") {
     fun syncRedditPosts() {
         val guilds = ayla.guildsColl.find().filter { it.redditSubs.isNotEmpty() }
 
-        guilds.forEach {
-            it.redditSubs.forEach { subReddit, channelId ->
-                val request = HttpRequest.get("https://reddit.com/r/$subReddit/.json")
-                        .userAgent(Constants.USER_AGENT)
+        async {
+            guilds.forEach {
+                it.redditSubs.forEach { subReddit, channelId ->
+                    val request = HttpRequest.get("https://reddit.com/r/$subReddit/.json")
+                            .userAgent(Constants.USER_AGENT)
 
-                if (request.code() != 200) {
-                    throw RuntimeException("Request code for https://reddit.com/r/$subReddit/.json is not 200!")
-                }
+                    if (request.code() != 200) {
+                        throw RuntimeException("Request code for https://reddit.com/r/$subReddit/.json is not 200!")
+                    }
 
-                val payload = JsonParser().parse(request.body())
-                val data = payload["data"].obj
+                    val payload = JsonParser().parse(request.body())
+                    val data = payload["data"].obj
 
-                val children = data["children"].array
+                    val children = data["children"].array
 
-                for (child in children) {
-                    val comment = child["data"]
+                    for (child in children) {
+                        val comment = child["data"]
 
-                    if (!comment["stickied"].bool) {
-                        if (comment["created"].long > (it.lastRedditPostCreation[subReddit] ?: "0").toLong()) { // É um post novo!
-                            val guildHandle = ayla.getGuildById(it.id) ?: return@forEach
+                        if (!comment["stickied"].bool) {
+                            if (comment["created"].long > (it.lastRedditPostCreation[subReddit]
+                                        ?: "0").toLong()) { // É um post novo!
+                                val guildHandle = ayla.getGuildById(it.id) ?: return@forEach
 
-                            val channel = guildHandle.getTextChannelById(channelId) ?: return@forEach
+                                val channel = guildHandle.getTextChannelById(channelId) ?: return@forEach
 
-                            val builder = EmbedBuilder()
+                                val builder = EmbedBuilder()
 
-                            builder.setAuthor(comment["title"].string, "https://reddit.com" + comment["permalink"].string, "https://pbs.twimg.com/profile_images/868147475852312577/fjCSPU-a_400x400.jpg")
-                            builder.setColor(Constants.REDDIT_ORANGE_RED)
+                                builder.setAuthor(comment["title"].nullString ?: "Sem título", "https://reddit.com" + comment["permalink"].nullString, "https://pbs.twimg.com/profile_images/868147475852312577/fjCSPU-a_400x400.jpg")
+                                builder.setColor(Constants.REDDIT_ORANGE_RED)
 
-                            val content = comment["selftext"].nullString
-                            if (content != null) {
-                                val contentSubstringed = if (content.length > 2000) {
-                                    content.substring(0, 2030) + "... [Leia mais aqui](https;//reddit.com${comment["permalink"].string})"
-                                } else {
-                                    content
+                                val content = comment["selftext"].nullString
+                                if (content != null) {
+                                    val contentSubstringed = if (content.length > 2000) {
+                                        content.substring(0, 2030) + "... [Leia mais aqui](https;//reddit.com${comment["permalink"].string})"
+                                    } else {
+                                        content
+                                    }
+
+                                    builder.setDescription(contentSubstringed)
                                 }
 
-                                builder.setDescription(contentSubstringed)
-                            }
+                                try {
+                                    val images = comment["preview"].obj["images"].array.iterator().next()
+                                    val imageContent = images.obj
 
-                            try {
-                                val images = comment["preview"].obj["images"].array.iterator().next()
-                                val imageContent = images.obj
+                                    val url = comment["url"].nullString
 
-                                if (images != null) {
-                                    val imageUrl = imageContent["source"].obj["url"].string
+                                    if (images != null) {
+                                        val imageUrl = imageContent["source"].obj["url"].string
 
-                                    builder.setImage(imageUrl)
+                                        builder.setImage(imageUrl)
+                                    } else if (url != null) {
+                                        builder.setImage(url)
+                                    }
+                                } catch (e: NoSuchElementException) {
+                                    // Não tem imagens
                                 }
-                            } catch (e: NoSuchElementException) {
-                                // Não tem imagens
+
+                                builder.setFooter("u/" + comment["author"].string, null)
+                                builder.setTimestamp(OffsetDateTime.now())
+
+                                channel.sendMessage(builder.build()).complete()
+
+                                it.lastRedditPostCreation[subReddit] = comment["created"].long.toString()
+                                guildHandle.config = it
                             }
 
-                            builder.setFooter("u/" + comment["author"].string, null)
-                            builder.setTimestamp(OffsetDateTime.now())
-
-                            channel.sendMessage(builder.build()).complete()
-
-                            it.lastRedditPostCreation[subReddit] = comment["created"].long.toString()
-                            guildHandle.config = it
+                            break
                         }
-
-                        break
                     }
                 }
             }

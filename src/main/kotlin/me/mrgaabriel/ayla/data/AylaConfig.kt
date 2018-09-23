0523154e29c193d2249b8274331dbb1877a8274d
Gabriel <gabrielbellot7@gmail.com>
2018-09-23 17:42:44 -0300

    Enviar dados para o Bots on Discord

--- src/main/kotlin/me/mrgaabriel/ayla/data/AylaConfig.kt
+++ src/main/kotlin/me/mrgaabriel/ayla/data/AylaConfig.kt
@@ -12,4 +12,5 @@ class AylaConfig(val clientToken: String = "Token do Bot",
                  val discordBotListToken: String = "Token do Discord Bot List",
                  val mongoHostname: String = "Hostname do MongoDB",
                  val mongoDatabaseName: String = "Nome do banco de dados do MongoDB",
-                 val youtubeApiKey: String = "API key do YouTube")
+                 val youtubeApiKey: String = "API key do YouTube",
+                 val botsOnDiscordToken: String = "Token do Bots on Discord")

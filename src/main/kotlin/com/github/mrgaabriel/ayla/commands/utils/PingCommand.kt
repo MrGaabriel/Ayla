e1f6591c061dbeb03b58c81923697c4eab845934
MrGaabriel <hello@mrgaabriel.space>
2018-11-24 00:23:24 +0000

    Calcular API Ping no "..ping"

--- src/main/kotlin/com/github/mrgaabriel/ayla/commands/utils/PingCommand.kt
+++ src/main/kotlin/com/github/mrgaabriel/ayla/commands/utils/PingCommand.kt
@@ -2,10 +2,14 @@ package com.github.mrgaabriel.ayla.commands.utils
 
 import com.github.mrgaabriel.ayla.commands.AbstractCommand
 import com.github.mrgaabriel.ayla.commands.CommandContext
+import com.github.mrgaabriel.ayla.utils.extensions.await
 
 class PingCommand : AbstractCommand("ping") {
 
     override suspend fun run(context: CommandContext) {
-        context.sendMessage("${context.event.author.asMention} **Pong!** `${context.event.jda.ping}ms` :ping_pong:")
+        val start = System.currentTimeMillis()
+        val message = context.sendMessage("${context.event.author.asMention} **Pong!** :ping_pong:\nGateway Ping: `${context.event.jda.ping}ms`")
+
+        message.editMessage("${context.event.author.asMention} **Pong!** :ping_pong:\nGateway Ping: `${context.event.jda.ping}ms`\nAPI Ping: `${System.currentTimeMillis() - start}ms`").await()
     }
 }

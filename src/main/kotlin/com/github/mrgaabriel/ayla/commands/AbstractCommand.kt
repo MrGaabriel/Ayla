ca59d8cfdd1278e748d7770f0f5a5b9c9393c5f0
MrGaabriel <hello@mrgaabriel.space>
2018-12-06 15:01:55 -0200

    .queue()

--- src/main/kotlin/com/github/mrgaabriel/ayla/commands/AbstractCommand.kt
+++ src/main/kotlin/com/github/mrgaabriel/ayla/commands/AbstractCommand.kt
@@ -66,7 +66,7 @@ abstract class AbstractCommand(val label: String, val aliases: List<String> = li
                 event.channel.sendTyping().queue()
 
                 if (commandCooldownCache.getOrDefault(event.author.id, 0) > System.currentTimeMillis()) {
-                    event.channel.sendMessage("${event.author.asMention} Espere **${DateUtils.formatDateDiff(commandCooldownCache[event.author.id]!!)} para poder utilizar este comando novamente!")
+                    event.channel.sendMessage("${event.author.asMention} Espere **${DateUtils.formatDateDiff(commandCooldownCache[event.author.id]!!)} para poder utilizar este comando novamente!").queue()
                     commandCooldownCache[event.author.id] = commandCooldownCache[event.author.id]!! + 500
                     return true
                 }

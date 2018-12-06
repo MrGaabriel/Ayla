0006cf0f2287b5451640304382257e1e1021fd1d
MrGaabriel <hello@mrgaabriel.space>
2018-12-06 12:17:40 -0200

    Verificar se o sub-reddit existe e se está marcado como NSFW

--- src/main/kotlin/com/github/mrgaabriel/ayla/commands/config/RedditCommand.kt
+++ src/main/kotlin/com/github/mrgaabriel/ayla/commands/config/RedditCommand.kt
@@ -1,10 +1,16 @@
 package com.github.mrgaabriel.ayla.commands.config
 
+import com.github.kevinsawicki.http.HttpRequest
 import com.github.mrgaabriel.ayla.commands.AbstractCommand
 import com.github.mrgaabriel.ayla.commands.CommandContext
 import com.github.mrgaabriel.ayla.utils.AylaUtils
+import com.github.mrgaabriel.ayla.utils.Constants
 import com.github.mrgaabriel.ayla.utils.RedditUtils
+import com.github.mrgaabriel.ayla.utils.Static
 import com.github.mrgaabriel.ayla.utils.extensions.ayla
+import com.github.salomonbrys.kotson.bool
+import com.github.salomonbrys.kotson.int
+import com.github.salomonbrys.kotson.obj
 import net.dv8tion.jda.core.Permission
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -29,6 +35,34 @@ class RedditCommand : AbstractCommand("reddit") {
         }
 
         val arg0 = context.args[0]
+
+        val body = HttpRequest.get("https://www.reddit.com/subreddits/search.json?q=$arg0")
+            .userAgent(Constants.USER_AGENT)
+            .body()
+
+        val payload = Static.JSON_PARSER.parse(body).obj
+        val data = payload["data"].obj
+
+        val dist = data["dist"].int
+
+        if (dist == 0) {
+            context.sendMessage("${context.event.author.asMention} Este sub-reddit não existe!")
+            return
+        }
+
+        val about = HttpRequest.get("https://reddit.com/r/$arg0/about.json")
+            .userAgent(Constants.USER_AGENT)
+            .body()
+
+        val aboutPayload = Static.JSON_PARSER.parse(about).obj
+        val aboutData = aboutPayload["data"].obj
+
+        val nsfw = aboutData["over18"].bool
+        if (nsfw) {
+            context.sendMessage("${context.event.author.asMention} O sub-reddit `/r/$arg0` está marcado como NSFW... e eu não quero ficar postando coisas desse tipo aqui. :rolling_eyes:")
+            return
+        }
+
         val sub = RedditUtils.getOrCreateSubReddit(arg0.toLowerCase())
 
         val arg1 = context.args[1]

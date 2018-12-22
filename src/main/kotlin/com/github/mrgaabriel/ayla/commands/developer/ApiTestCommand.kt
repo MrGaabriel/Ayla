9de894515567eacba154d44d974d8d3cea97e8c1
MrGaabriel <hello@mrgaabriel.space>
2018-12-22 18:30:00 -0200

    :facepalm:

--- src/main/kotlin/com/github/mrgaabriel/ayla/commands/developer/ApiTestCommand.kt
+++ src/main/kotlin/com/github/mrgaabriel/ayla/commands/developer/ApiTestCommand.kt
@@ -1,6 +1,7 @@
 package com.github.mrgaabriel.ayla.commands.developer
 
 import com.github.mrgaabriel.ayla.commands.AylaCommand
+import com.github.mrgaabriel.ayla.commands.AylaCommandContext
 import com.github.mrgaabriel.ayla.commands.CommandContext
 import net.dv8tion.jda.core.entities.User
 import net.perfectdreams.commands.annotation.Subcommand
@@ -8,12 +9,12 @@ import net.perfectdreams.commands.annotation.Subcommand
 class ApiTestCommand : AylaCommand("apitest", "testando") {
 
     @Subcommand
-    suspend fun root(context: CommandContext) {
+    suspend fun root(context: AylaCommandContext) {
         context.reply("hello world!")
     }
 
     @Subcommand(["mentionuser"])
-    suspend fun mentionUser(context: CommandContext, user: User) {
+    suspend fun mentionUser(context: AylaCommandContext, user: User) {
         context.reply("o usuário é ${user.asMention}")
     }
 }

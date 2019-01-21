c2e052174cfb4edd7c97f39287898bc291df6c24
MrGaabriel <hello@mrgaabriel.space>
2019-01-21 13:19:49 -0200

    Whoops

--- src/main/kotlin/com/github/mrgaabriel/ayla/commands/utils/PingCommand.kt
+++ src/main/kotlin/com/github/mrgaabriel/ayla/commands/utils/PingCommand.kt
@@ -1,11 +1,18 @@
 package com.github.mrgaabriel.ayla.commands.utils
 
 import com.github.mrgaabriel.ayla.commands.AylaCommand
+import com.github.mrgaabriel.ayla.commands.CommandCategory
 import com.github.mrgaabriel.ayla.commands.CommandContext
 import com.github.mrgaabriel.ayla.utils.extensions.await
 import net.perfectdreams.commands.annotation.Subcommand
 
 class PingCommand : AylaCommand("ping") {
+
+    override val category: CommandCategory
+        get() = CommandCategory.UTILS
+
+    override val description: String
+        get() = "Vê o ping da conexão do bot com o Discord"
 
     @Subcommand
     suspend fun root(context: CommandContext) {
